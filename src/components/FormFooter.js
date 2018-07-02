fa3f7ed046eca04865472a19df6d062274259e5e
mouhssine idrissi akhelij <mouhssineidrissiakhelij1982@gmail.com>
2018-07-02 09:35:21 +0300

    shorten the code

--- src/components/FormFooter.js
+++ src/components/FormFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FormFooter = props => {
     return(
         <div className='header-container'>
-			<h2>&#169; COPYRIGHT 2018 MOUHSSINE</h2>
+			<h2>&#169; COPYRIGHT 2018 MOUHSSINE IDRISSI</h2>
 		</div>
     );
 }
